17467bcdb8e308fb4eb7e2b07543d27947dcec0b
solyrion <sde101573@gmail.com>
2024-08-21 15:06:27 +0900

    fix: MovieEntity 수정

--- Movie/src/main/java/com/example/movie/Entity/MovieEntity.java
+++ Movie/src/main/java/com/example/movie/Entity/MovieEntity.java
@@ -3,7 +3,12 @@ package com.example.movie.Entity;
 import com.example.movie.dto.MovieDTO;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Entity
@@ -14,7 +19,8 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "movie_table") //database에 해당 이름의 테이블 생성
-public class MovieEntity { //table 역할
+@EntityListeners(AuditingEntityListener.class)
+public class MovieEntity extends BaseTime{ //table 역할
     //jpa ==> database를 객체처럼 사용 가능
 
     @Id
@@ -30,6 +36,23 @@ public class MovieEntity { //table 역할
     @ManyToMany
     private Set<GenresEntity> genres;
 
+    @Column(length = 500)
+    private String body;
+
+    @CreatedDate
+    private LocalDateTime releaseDate;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "movie")
+    private List<Review> reviews = new ArrayList<>();
+
+    public void addReview(Review review){
+        reviews.add(review);
+        review.setMovie(this);
+    }
     public MovieEntity toMovieData() {
         return MovieEntity.builder().id(this.id).movieName(this.movieName).rating(this.rating).build();
     }
