b01b9bdbb36eeb3dbb219e57648a22e6b15a3ee3
solyrion <sde101573@gmail.com>
2024-08-25 21:26:10 +0900

    fix: 인터셉터 수정 및 적용

--- Movie/src/main/java/com/example/movie/WebConfig.java
+++ Movie/src/main/java/com/example/movie/WebConfig.java
@@ -7,16 +7,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-//임시로 주석처리함
-   /* @Override
+   @Override
     public void addInterceptors(InterceptorRegistry registry) {
 
         registry.addInterceptor(new LoginInterceptor())
                 .order(1)
                 .addPathPatterns("/**")
+                // 로그인 상관 없이 접속 가능한 url 목록 / 테스트 시 포합시켜서
                 .excludePathPatterns("/", "/members/add", "/login", "/logout", "/review/**",
                         "/css/**", "/*.ico", "/error",
-                        "/movie", "/movieinfo", "/");
+                        "/movie", "movieinfo", "/");
     }
-    */
+
 }
