422047aef800bbc84169b8b4506a23be4866f8ed
HOIVVVV <parkerpark@naver.com>
2024-09-05 15:46:10 +0900

    1
    1

--- Movie/src/main/java/com/example/movie/login/MemberController.java
+++ Movie/src/main/java/com/example/movie/login/MemberController.java
@@ -2,9 +2,11 @@ package com.example.movie.login;
 
 import com.example.movie.Entity.Member;
 import com.example.movie.Service.MemberService;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
